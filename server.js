577274d87b70d48bd7d9923c09eec44f8a534c48
Renatoxt <renatox.t1@outlook.com.br>
2021-10-24 17:03:08 -0300

    fix: configs de deploy no heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static('./dist/speech-to-libras'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/speech-to-libras/'}),
